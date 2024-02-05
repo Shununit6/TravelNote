5b1048e5a302072b799da36d9300ed76e0500597
Shununit6 <unit6xuli@gmail.com>
2024-02-04 20:59:30 -0800

    plan_form

--- app/forms/plan_form.py
+++ app/forms/plan_form.py
@@ -4,10 +4,10 @@ from wtforms.validators import DataRequired, NumberRange
 
 class PlanForm(FlaskForm):
     name = StringField('Plan Name', validators=[DataRequired(message='Plan Name is required')])
-    number_traveler =IntegerField('Number of Travelers', validators=[NumberRange(min=1, message='Number of travelers must be at lease 1')])
-    private = BooleanField('Private', validators=[DataRequired(message='Private is required')])
+    number_traveler =IntegerField('Number of Travelers', validators=[NumberRange(min=1, message='Number of travelers must be at least 1')])
+    private = BooleanField('Private', default=True)
     city = StringField('City', validators=[DataRequired(message='City is required')])
     country = StringField('Country', validators=[DataRequired(message='Country is required')])
-    start_date = DateField('Start Date', validators=[DataRequired(message='Start Date is required')])
-    end_date = DateField('End Date', validators=[DataRequired(message='End Date is required')])
+    start_date = DateField('Start Date', validators=[DataRequired(message='Start Date is required in yyyy-mm-dd format')])
+    end_date = DateField('End Date', validators=[DataRequired(message='End Date is required in yyyy-mm-dd format')])
     submit = SubmitField('Submit')
