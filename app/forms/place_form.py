df7beb7fb87586b85288067a82c014b61b271573
Shununit6 <unit6xuli@gmail.com>
2024-02-05 20:32:34 -0800

    place_form

--- app/forms/place_form.py
+++ app/forms/place_form.py
@@ -4,6 +4,6 @@ from wtforms.validators import DataRequired
 
 class PlaceForm(FlaskForm):
     name = StringField('Place Name', validators=[DataRequired(message='Place Name is required')])
-    type = SelectField('Place Type', validators=[DataRequired(message='Place Type is required')])
+    type = StringField('Place Type', validators=[DataRequired(message='Place Type is required')])
     description = StringField('Place Description', validators=[DataRequired(message='Place Description is required')])
     submit = SubmitField('Submit')
