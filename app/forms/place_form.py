9d701d9ba18d6efa8536efb7db397d349898679e
Shununit6 <unit6xuli@gmail.com>
2024-02-05 19:34:48 -0800

    place_form

---
+++ app/forms/place_form.py
@@ -0,0 +1,9 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, SelectField, SubmitField
+from wtforms.validators import DataRequired
+
+class PlaceForm(FlaskForm):
+    name = StringField('Place Name', validators=[DataRequired(message='Place Name is required')])
+    type = SelectField('Place Type', validators=[DataRequired(message='Place Type is required')])
+    description = StringField('Place Description', validators=[DataRequired(message='Place Description is required')])
+    submit = SubmitField('Submit')
