653d1e6176b615d849da573b5818d7683a10ae6b
Shununit6 <unit6xuli@gmail.com>
2024-02-09 15:50:01 -0800

    planindexitem

---
+++ react-vite/src/components/PlanForm/PlanForm.jsx
@@ -0,0 +1,252 @@
+import { useState, useEffect } from "react";
+import { useHistory,} from "react-router-dom"; //useParams,
+import { useDispatch, useSelector} from "react-redux"; //useSelector
+import { createPlan, updatePlan } from "../../redux/plans";
+import "./PlanForm.css";
+
+const PlanForm = ({ plan, formType }) => {
+    const dispatch = useDispatch();
+    const history = useHistory();
+    const sessionUser = useSelector(state => state.session.user);
+    const plans = useSelector(state => state.plans);
+    let [name, setName] = useState(plan?.name);
+    let [number_traveler, setNumber_Traveler] = useState(plan?.number_traveler);
+    let [city, setCity] = useState(plan?.city);
+    let [country, setCountry] = useState(plan?.country);
+    let [startDate, setStartDate] = useState(plan?.start_date);
+    let [endDate, setEndDate] = useState(plan?.end_date);
+    let privateState;
+    let userId = sessionUser.id;
+    if(plan?.private === 1){
+        privateState="Private";
+    }else if(plan?.private === 0){
+        privateState="Public";
+    }else{
+        privateState="";
+    }
+    let [isPrivate, setIsPrivate] = useState(privateState);
+
+    let isUpdate = false;
+    if(formType === "Update Plan"){
+        isUpdate = true;
+    }
+
+    const [validationErrors, setValidationErrors] = useState({});
+    const [hasSubmitted, setHasSubmitted] = useState(false);
+    console.log(plans);
+    console.log(userId)
+
+    useEffect(() => {
+        const errors = { name: [], number_traveler: [], isPrivate:[], city: [], country:[], startDate:[], endDate:[] };
+        if (!name.length) errors["name"].push("Name is required");
+        if (name.length > 60) errors["name"].push("Name must be 60 characters or less");
+        if (number_traveler < 1) errors["number_traveler"].push("");
+        if (!isPrivate.length) errors["isPrivate"].push("Visibility Type is required");
+        if (!city.length) errors["city"].push("City is required");
+        if (!country.length) errors["country"].push("Country is required");
+        if (!startDate.length) errors["startDate"].push("Plan start date is required");
+        if (new Date(`${new Date()}`).getTime() > new Date(startDate).getTime()) errors["startDate"].push("Start date must be in the future");
+        if (!endDate.length) errors["endDate"].push("Plan end date is required");
+        if (new Date(`${endDate}`).getTime() < new Date(startDate).getTime()) errors["endDate"].push("End date is less than start date");
+
+        setValidationErrors(errors);
+    }, [name, number_traveler, isPrivate, city, country, startDate, endDate]);
+
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        setHasSubmitted(true);
+
+        if(isPrivate === "Private"){
+            isPrivate = 1;
+        }else{
+            isPrivate = 0;
+        }
+        plan = { ...plan,  name, number_traveler, private:isPrivate, city, country, startDate, endDate };
+
+        let newPlan;
+        let errorCount = validationErrors.name.length + validationErrors.number_traveler.length
+        + validationErrors.isPrivate.length + validationErrors.city.length
+        + validationErrors.country.length + validationErrors.startDate.length
+        + validationErrors.endDate.length;
+        // console.log(errorCount);
+        if (errorCount > 0){
+            // console.log("has errors");
+            }else{
+                // console.log("no errors");
+                if (formType === "Update Plan") {
+                    newPlan = await dispatch(updatePlan(plan));
+                } else {
+                    newPlan = await dispatch(createPlan(plan));
+                }
+                if (newPlan.id) {
+                    // console.log("newPlan.id", newPlan.id);
+                    history.push(`/plans/${newPlan.id}`);
+                } else {
+                    const { validationErrors } = await newPlan.json();
+                    setValidationErrors(validationErrors);
+                }
+                // console.log(newPlan);
+
+                setName('');
+                setNumber_Traveler('');
+                setIsPrivate('');
+                setCity('');
+                setCountry('');
+                setStartDate('');
+                setEndDate('');
+                setValidationErrors({});
+                setHasSubmitted(false);
+            }
+    };
+
+//     /* **DO NOT CHANGE THE RETURN VALUE** */
+    return (
+        <form onSubmit={handleSubmit}>
+            {/* {console.log(validationErrors)} */}
+            <div id="planformcreateupdate">
+                <div id="titlecreateupdategroupform">
+                    {!isUpdate && <h2>Start a New Plan</h2>}
+                    {isUpdate && <h2>Update your Plan</h2>}
+                </div>
+            <div>
+                <label>
+                    <input
+                        id='planformname'
+                        type="text"
+                        placeholder="What is your plan name?"
+                        onChange={(e) => setName(e.target.value)}
+                        value={name}
+                    />
+                    {hasSubmitted &&
+                        validationErrors.name.length > 0 &&
+                        validationErrors.name.map((error, idx) => (
+                            <div key={idx}>
+                                <p className="error">{error}</p>
+                            </div>
+                        ))}
+                </label>
+            </div>
+            <div>
+                <label>
+                    <textarea
+                        id='planformnumber'
+                        value={number_traveler}
+                        placeholder="Please enter a number larger than 0"
+                        onChange={(e) => setNumber_Traveler(e.target.value)}
+                    />
+                    {hasSubmitted &&
+                        validationErrors.number_traveler.length > 0 &&
+                        validationErrors.number_traveler.map((error, idx) => (
+                            <div key={idx}>
+                                <p className="error">{error}</p>
+                            </div>
+                        ))}
+                </label>
+            </div>
+            <div>
+                <label>
+                    <p>Is this a private or public plan?</p>
+                    <select id="private" value={isPrivate} onChange={(e) => setIsPrivate(e.target.value)}>
+                        <option value='' disabled>(select one)</option>
+                        <option value="Private">Private</option>
+                        <option value="Public">Public</option>
+                    </select>
+                    {hasSubmitted &&
+                        validationErrors.private.length > 0 &&
+                        validationErrors.private.map((error, idx) => (
+                            <div key={idx}>
+                                <p className="error">{error}</p>
+                            </div>
+                        ))}
+                </label>
+            </div>
+            <div>
+                <label>
+                    <input
+                        id='planformcity'
+                        type="text"
+                        placeholder="Please add the city"
+                        onChange={(e) => setCity(e.target.value)}
+                        value={city}
+                    />
+                    {hasSubmitted &&
+                        validationErrors.city.length > 0 &&
+                        validationErrors.city.map((error, idx) => (
+                            <div key={idx}>
+                                <p className="error">{error}</p>
+                            </div>
+                        ))}
+                </label>
+            </div>
+            <div>
+                <label>
+                    <input
+                        id='planformcity'
+                        type="text"
+                        placeholder="Please add the country"
+                        onChange={(e) => setCountry(e.target.value)}
+                        value={country}
+                    />
+                    {hasSubmitted &&
+                        validationErrors.country.length > 0 &&
+                        validationErrors.country.map((error, idx) => (
+                            <div key={idx}>
+                                <p className="error">{error}</p>
+                            </div>
+                        ))}
+                </label>
+            </div>
+            <div>
+                <label>
+                     When does your trip start?
+                     <div>
+                        <input
+                            id='planformstartDate'
+                            type="datetime-local"
+                            name="startDate"
+                            placeholder="MM/DD/YYYY, HH/mm AM"
+                            onChange={(e) => setStartDate(e.target.value)}
+                            value={startDate}
+                            min={Date()}
+                        />
+                        {hasSubmitted &&
+                            validationErrors.startDate.length > 0 &&
+                            validationErrors.startDate.map((error, idx) => (
+                                <div key={idx}>
+                                    <p className="error">{error}</p>
+                                </div>
+                            ))}
+                     </div>
+                 </label>
+            </div>
+            <div>
+                <label>
+                    When does your trip end?
+                    <div>
+                        <input
+                            id='planformendDate'
+                            type="datetime-local"
+                            name="endDate"
+                            placeholder="MM/DD/YYYY, HH/mm PM"
+                            onChange={(e) => setEndDate(e.target.value)}
+                            value={endDate}
+                            min={startDate}
+                        />
+                        {hasSubmitted &&
+                            validationErrors.endDate.length > 0 &&
+                            validationErrors.endDate.map((error, idx) => (
+                                <div key={idx}>
+                                    <p className="error">{error}</p>
+                                </div>
+                            ))}
+                    </div>
+                </label>
+            </div>
+            <button type="submit" id="GroupCreateUpdateButton" >{formType}</button>
+            </div>
+        </form>
+    );
+};
+
+export default PlanForm;
---
+++ react-vite/src/components/Plans/Plans.jsx
@@ -0,0 +1,37 @@
+import React, { useEffect, useState } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { Link } from 'react-router-dom';
+import { getAllPlans } from '../../redux/plans';
+import PlanIndexItem from '../PlanIndexItem';
+import "./plans.css";
+
+function Plans() {
+    const dispatch = useDispatch();
+    const [isLoaded, setIsLoaded] = useState(false);
+    const plans = useSelector((state) => state.plans);
+    useEffect(()=>{
+      dispatch(getAllPlans()).then(()=>setIsLoaded(true))
+    }, [dispatch]);
+
+  if (!isLoaded) {
+    return (<div>Loading...</div>);
+  }
+  // console.log(plans);
+  if(isLoaded){
+  return (
+    <div id="planslistgrid">
+        <h2>
+            {/* <Link id="eventsIsNotActive" to="/events" > Events </Link> */}
+        <Link id="plansIsActive" to="/plans" > Plans </Link>
+        </h2>
+         <p>All Trip Plans</p>
+         <div id="viewallplans">
+            {Object.values(plans).map((plan, index) => (
+                  <PlanIndexItem plan={plan} key={index}/>
+            ))}
+         </div>
+    </div>
+  );}
+}
+
+export default Plans;
