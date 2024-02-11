84beb835f94236f0c94631458d4a07ca9d3d8050
Shununit6 <unit6xuli@gmail.com>
2024-02-10 21:40:16 -0800

    planindexitem

---
+++ react-vite/src/components/PlanIndexItem/PlanIndexItem.jsx
@@ -0,0 +1,36 @@
+import { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { getPlanDetails } from "../../redux/plans";
+import { Link } from 'react-router-dom';
+import "./PlanIndexItem.css";
+const PlanIndexItem = ({ plan }) => {
+    const dispatch = useDispatch();
+    const sessionUser = useSelector(state => state.session.user);
+    // const { id, user_id, name, number_traveler, city, country, start_date, end_date, created_at, updated_at} = plan;
+    const { id, name, number_traveler, city, country, start_date, end_date, created_at, updated_at} = plan;
+
+    return (
+        <Link id="planlinkwithtext" to={`/plans/${id}`} key={`${id}`}>
+            <div id="plangrid1">
+                {/* <div id="planitem1">
+                    <img id="planExpenseImage" src={url} alt="planExpenseImage" />
+                </div> */}
+                <div id="planitem2">
+                    {id}
+                    {name}
+                    {number_traveler}
+                    {city}
+                    {country}
+                    {start_date}
+                    {end_date}
+                    {created_at}
+                    {updated_at}
+                </div>
+            </div>
+        </Link>
+
+
+    );
+}
+
+export default AlbumIndexItem;
