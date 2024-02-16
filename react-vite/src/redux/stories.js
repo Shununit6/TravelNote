f0d6e1d62a3d7a332e2cd1cbdba40e33a645f008
Shununit6 <unit6xuli@gmail.com>
2024-02-15 19:46:30 -0800

    storiesredux

--- react-vite/src/redux/stories.js
+++ react-vite/src/redux/stories.js
@@ -1,139 +1,139 @@
-// // /** Action Type Constants: */
-// export const LOAD_STORIES = "stories/LOAD_STORIES";
-// export const LOAD_STORY_DETAILS = "stories/LOAD_STORIES_DETAILS";
-// export const RECEIVE_PLAN = "stories/RECEIVE_PLAN";
-// export const UPDATE_PLAN = "stories/UPDATE_PLAN";
-// export const REMOVE_PLAN = "stories/REMOVE_PLAN";
+// /** Action Type Constants: */
+export const LOAD_STORIES = "stories/LOAD_STORIES";
+export const LOAD_STORY_DETAILS = "stories/LOAD_STORIES_DETAILS";
+export const RECEIVE_STORY = "stories/RECEIVE_STORY";
+export const UPDATE_STORY = "stories/UPDATE_STORY";
+export const REMOVE_STORY = "stories/REMOVE_STORY";
 
-// // /**  Action Creators: */
-// export const loadStories = (stories) => ({
-//     type: LOAD_STORIES,
-//     stories,
-// });
+// /**  Action Creators: */
+export const loadStories = (stories) => ({
+    type: LOAD_STORIES,
+    stories,
+});
 
-// export const loadPlanDetails = (plan) => ({
-//     type: LOAD_PLAN_DETAILS,
-//     plan,
-// });
+export const loadStoryDetails = (story) => ({
+    type: LOAD_STORY_DETAILS,
+    story,
+});
 
-// export const receivePlan = (plan) => ({
-//     type: RECEIVE_PLAN,
-//     plan,
-// });
+export const receiveStory = (story) => ({
+    type: RECEIVE_STORY,
+    story,
+});
 
-// export const editPlan = (plan) => ({
-//     type: UPDATE_PLAN,
-//     plan,
-// });
+export const editStory = (story) => ({
+    type: UPDATE_STORY,
+    story,
+});
 
-// export const removePlan = (plan) => ({
-//     type: REMOVE_PLAN,
-//     plan,
-// });
+export const removeStory = (story) => ({
+    type: REMOVE_STORY,
+    story,
+});
 
-// // /** Thunk Action Creators: */
-// export const getAllPlans = () => async (dispatch) => {
-//     const res = await fetch(`/api/plans`);
+// /** Thunk Action Creators: */
+export const getAllStories = () => async (dispatch) => {
+    const res = await fetch(`/api/stories`);
 
-//     if (res.ok) {
-//         const data = await res.json();
-//         // console.log("data", data);
-//         dispatch(loadPlans(data));
-//         return data;
-//     }
-//     return res;
-// };
+    if (res.ok) {
+        const data = await res.json();
+        // console.log("data", data);
+        dispatch(loadStories(data));
+        return data;
+    }
+    return res;
+};
 
-// export const getPlanDetails = (planId) => async dispatch => {
-//     // console.log("Fetching plan details for planId:", planId);
-//     const res = await fetch(`/api/plans/${planId}`);
+export const getStoryDetails = (storyId) => async dispatch => {
+    // console.log("Fetching story details for storyId:", storyId);
+    const res = await fetch(`/api/stories/${storyId}`);
 
-//     if (res.ok) {
-//         const data = await res.json();
-//         // console.log("Received data:", data);
-//         dispatch(loadPlanDetails(data));
-//         return data;
-//     }
-//     // console.log("Error fetching plan details:", res.statusText);
-//     return res;
-// };
+    if (res.ok) {
+        const data = await res.json();
+        // console.log("Received data:", data);
+        dispatch(loadStoryDetails(data));
+        return data;
+    }
+    // console.log("Error fetching story details:", res.statusText);
+    return res;
+};
 
-// export const getMyPlans = () => async (dispatch) => {
-//     const res = await fetch('/api/plans/current');
-//     if (res.ok) {
-//         const data = await res.json();
-//         dispatch(loadPlans(data));
-//         return data;
-//     }
-//     return res;
-// };
+export const getMyStories = () => async (dispatch) => {
+    const res = await fetch('/api/stories/current');
+    if (res.ok) {
+        const data = await res.json();
+        dispatch(loadStories(data));
+        return data;
+    }
+    return res;
+};
 
-// export const createPlan = (payload) => async (dispatch) => {
-//     const res = await fetch("/api/plans/", {
-//         method: "POST",
-//         headers: { "Content-Type": "application/json" },
-//         body: JSON.stringify(payload),
-//     });
+export const createStory = (payload) => async (dispatch) => {
+    const res = await fetch("/api/stories/", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(payload),
+    });
 
-//     if (res.ok) {
-//         const data = await res.json();
-//         dispatch(receivePlan(data));
-//         return data;
-//     }
-//     return res;
-// };
+    if (res.ok) {
+        const data = await res.json();
+        dispatch(receiveStory(data));
+        return data;
+    }
+    return res;
+};
 
-// export const updatePlan = (plan) => async (dispatch) => {
-//     const res = await fetch(`/api/plans/${plan.id}`, {
-//         method: "PUT",
-//         headers: { "Content-Type": "application/json" },
-//         body: JSON.stringify(plan),
-//     });
-//     if (res.ok) {
-//         const data = await res.json();
-//         dispatch(editPlan(data));
-//         return data;
-//     }
-//     return res;
-// };
+export const updateStory = (story) => async (dispatch) => {
+    const res = await fetch(`/api/stories/${story.id}`, {
+        method: "PUT",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(story),
+    });
+    if (res.ok) {
+        const data = await res.json();
+        dispatch(editStory(data));
+        return data;
+    }
+    return res;
+};
 
-// export const deletePlan = (planId) => async (dispatch) => {
-//     const res = await fetch(`/api/plans/${planId}`, {
-//         method: "DELETE",
-//     });
+export const deleteStory = (storyId) => async (dispatch) => {
+    const res = await fetch(`/api/stories/${storyId}`, {
+        method: "DELETE",
+    });
 
-//     if (res.ok) {
-//         const data = await res.json();
-//         dispatch(removePlan(planId));
-//         return data;
-//     }
-//     return res;
-// };
+    if (res.ok) {
+        const data = await res.json();
+        dispatch(removeStory(storyId));
+        return data;
+    }
+    return res;
+};
 
-// const plansReducer = (state = {}, action) => {
-//     switch (action.type) {
-//         case LOAD_PLANS: {
-//             const plansState = { ...state };
-//             action.plans.plans.forEach((plan) => {
-//                 if (!plansState[plan.id]) { plansState[plan.id] = plan; }
-//             });
-//             return {...plansState};
-//         }
-//         case LOAD_PLAN_DETAILS: {
-//             return { ...state, [action.plan.id]: action.plan };
-//         }
-//         case RECEIVE_PLAN:
-//             return { ...state, [action.plan.id]: action.plan };
-//         case UPDATE_PLAN:
-//             return { ...state };
-//         case REMOVE_PLAN: {
-//             const planState = { ...state };
-//             delete planState[action.plan];
-//             return {...planState};
-//         }
-//         default:
-//             return { ...state };
-//     }
-// };
+const storiesReducer = (state = {}, action) => {
+    switch (action.type) {
+        case LOAD_STORIES: {
+            const storiesState = { ...state };
+            action.stories.stories.forEach((story) => {
+                if (!storiesState[story.id]) { storiesState[story.id] = story; }
+            });
+            return {...storiesState};
+        }
+        case LOAD_STORY_DETAILS: {
+            return { ...state, [action.story.id]: action.story };
+        }
+        case RECEIVE_STORY:
+            return { ...state, [action.story.id]: action.story };
+        case UPDATE_STORY:
+            return { ...state };
+        case REMOVE_STORY: {
+            const storyState = { ...state };
+            delete storyState[action.story];
+            return {...storyState};
+        }
+        default:
+            return { ...state };
+    }
+};
 
-// export default plansReducer;
+export default storiesReducer;
