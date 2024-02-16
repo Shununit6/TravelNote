80be4092424446e12fbe80d8a39ae92013b98be9
Shununit6 <unit6xuli@gmail.com>
2024-02-15 22:37:57 -0800

    storydetail

--- react-vite/src/router/index.jsx
+++ react-vite/src/router/index.jsx
@@ -14,6 +14,7 @@ import CreatePlaceForm from '../components/CreatePlaceForm';
 import PlaceDetails from '../components/PlaceDetails';
 import ManagePlaces from '../components/ManagePlaces/ManagePlaces';
 import Stories from '../components/Stories';
+import StoryDetails from '../components/StoryDetails';
 
 export const router = createBrowserRouter([
   {
@@ -50,6 +51,10 @@ export const router = createBrowserRouter([
       {
         path: "places/:placeId",
         element: <PlaceDetails />,
+      },
+      {
+        path: "stories/:storyId",
+        element: <StoryDetails />,
       },
       {
         path: "plans",
