d9384dee1744c2ea2d4a53868eb683a32e0d6c57
Shununit6 <unit6xuli@gmail.com>
2024-03-13 19:38:17 -0700

    PlanIndexItem.jsx

--- react-vite/src/components/PlanIndexItem/PlanIndexItem.jsx
+++ react-vite/src/components/PlanIndexItem/PlanIndexItem.jsx
@@ -8,7 +8,7 @@ const PlanIndexItem = ({ plan, expense, manage }) => {
     // const dispatch = useDispatch();
     // const sessionUser = useSelector(state => state.session.user);
     // const { id, user_id, name, number_traveler, city, country, start_date, end_date, created_at, updated_at} = plan;
-    const { id, name } = plan;
+    const { id, name, number_traveler } = plan;
     const isPrivate = plan.private;
     const planexpenses = (id) =>{
         if(expense && Object.values(expense) && Object.values(expense).filter(expense=>expense.plan_id==id).length){
@@ -23,12 +23,19 @@ const PlanIndexItem = ({ plan, expense, manage }) => {
     data.push(["category", "amount"])
     if(expensedata){
         expensedata.forEach((x)=>{
-            if(datacategory.includes(x.category)){
+            if(datacategory.includes(x.category) && x.split==false){
                 dataamount[datacategory.indexOf(x.category)] += x.amount
             }
-            else{
+            if(!datacategory.includes(x.category) && x.split==false){
                 datacategory.push(x.category)
                 dataamount.push(x.amount)
+            }
+            if(datacategory.includes(x.category) && x.split==true){
+                dataamount[datacategory.indexOf(x.category)] += x.amount/number_traveler
+            }
+            if(!datacategory.includes(x.category) && x.split==true){
+                datacategory.push(x.category)
+                dataamount.push(x.amount/number_traveler)
             }
         })
         datacategory.forEach((x, index)=>{
