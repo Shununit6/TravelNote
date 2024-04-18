c4bf3234e780cac187cd64395e0de869b57ccd91
Shununit6 <unit6xuli@gmail.com>
2024-04-18 09:20:12 -0700

    navigation

--- react-vite/src/components/Navigation/Navigation.jsx
+++ react-vite/src/components/Navigation/Navigation.jsx
@@ -6,30 +6,26 @@ import navlogo from '../../../src/images/logo.png';
 function Navigation({ isLoaded }){
 	const sessionUser = useSelector(state => state.session.user);
 	return (
-		<nav><NavLink to="/"><img id="logoImage" src={navlogo} alt="logoimage"/></NavLink>
-			<div>
-			{/* {sessionUser &&
-			<div >
-				<NavLink to="/plans/current"><button>View my plans</button></NavLink>
-			</div>}
-			{sessionUser &&
-			<div>
-				<NavLink to="/places/current"><button>View my places</button></NavLink>
-			</div>}
-			{sessionUser &&
-			<div>
-				<NavLink to="/stories/current"><button>View my stories</button></NavLink>
-			</div>}
-			<div className="leftmenulibrarycreate-1">
-					<NavLink to="/plans"><button>View all the plans</button></NavLink>
-					<NavLink to="/places"><button>View all the places</button></NavLink>
-					<NavLink to="/stories"><button>View all the stories</button></NavLink>
-					</div> */}
-			{isLoaded && (
-				<ProfileButton user={sessionUser} />
-			)}
+
+			<div className="navbox">
+				<div><NavLink to="/"><img id="logoImage" src={navlogo} alt="logoimage"/></NavLink></div>
+				<div >
+					<NavLink to="/plans/"><button>View All Plans</button></NavLink>
+				</div>
+				{sessionUser &&
+				<div>
+					<NavLink to="/plans/current"><button>View My Plans</button></NavLink>
+				</div>}
+				{sessionUser &&
+				<div>
+					<NavLink to="/plans/new"><button>Create New Plans</button></NavLink>
+				</div>}
+				<div>
+					{isLoaded && (
+						<ProfileButton user={sessionUser} />
+					)}
+				</div>
 			</div>
-		</nav>
 	);
 }
 
